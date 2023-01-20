15cbf015e8f66fccee28eb402a525884c89c94bc
Ben Cerrone <bwc2184@rit.edu>
2023-01-19 20:37:57 -0500

    changed uart print formatting; removed some commented out lines

--- src/PreCharge/dev/MAX22530.cpp
+++ src/PreCharge/dev/MAX22530.cpp
@@ -4,7 +4,9 @@
 
 namespace PreCharge {
 
-    MAX22530::MAX22530(IO::SPI& SPI) : spi(SPI){}
+    MAX22530::MAX22530(IO::SPI& SPI) : spi(SPI) {
+
+    }
 
     uint8_t MAX22530::convertToVoltage(uint16_t count) {
         uint8_t result = ( ((uint32_t) count) * 1098) / 40960; //explain: 1.8 * 61 (* 10 for 1.8)
@@ -15,7 +17,8 @@ namespace PreCharge {
         uint16_t result;
         spi.readReg(0, reg, bytes, 2);
         result = bytes[0]<<8;
-        result|=bytes[1];
+        result |= bytes[1];
         return convertToVoltage(result);
     }
+
 }
--- targets/MAX22530/main.cpp
+++ targets/MAX22530/main.cpp
@@ -1,12 +1,8 @@
-/**
-* This is the primary State machine handler for the pre-charge board
-*/
 #include <EVT/io/UART.hpp>
 #include <EVT/io/manager.hpp>
 #include <EVT/io/pin.hpp>
 #include <EVT/utils/log.hpp>
 #include <PreCharge/dev/MAX22530.hpp>
-
 
 namespace IO = EVT::core::IO;
 namespace DEV = EVT::core::DEV;
@@ -19,22 +15,16 @@ IO::GPIO* devices[deviceCount];
 
 int main() {
    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+
    devices[0] = &IO::getGPIO<IO::Pin::SPI_CS>(EVT::core::IO::GPIO::Direction::OUTPUT);
    devices[0]->writePin(IO::GPIO::State::HIGH);
-   // Setup SPI
-   // need to figure out pins (need windows machine for the maxim board)
-   //
+
    IO::SPI& spi = IO::getSPI<IO::Pin::SPI_SCK, IO::Pin::SPI_MOSI, IO::Pin::SPI_MISO>(devices, deviceCount);
    spi.configureSPI(SPI_SPEED, SPI_MODE0, SPI_MSB_FIRST);
 
    PreCharge::MAX22530 MAX = PreCharge::MAX22530(spi);
 
-   //while(1){
-   //time::wait(1000);
-       for (int i=0; i<=0x14; i++) {
-           uart.printf("%x: %d\r\n", i, MAX.readVoltage(i<<2));
-        //   time::wait(10);
-       }
-
-   //}
+   for (int reg=0x01; reg<=0x08; reg++) {
+       uart.printf("0x%x: %d\r\n", reg, MAX.readVoltage(reg<<2));
+   }
 }
