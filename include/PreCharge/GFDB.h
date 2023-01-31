ebe109165d9e71bfbc65368d9458656db7888ba1
Aaron Chan <aarchan108@gmail.com>
2023-01-30 19:48:55 -0500

    Initial header file written fro GFDB

---
+++ include/PreCharge/GFDB.h
@@ -0,0 +1,42 @@
+/**
+ * SIM100 Driver for ground fault detection
+ */
+
+#ifndef PVC_GFDB_H
+#define PVC_GFDB_H
+
+namespace IO = EVT::core::IO;
+
+namespace GFDB {
+
+class GFDB {
+public:
+    const GFDB_ID = 0xA100101;
+
+    GFDB(IO::CAN& can);
+
+    IO::CAN::CANStatus init();
+
+    IO::CAN::CANStatus requestTemp(int8_t *temperature);
+
+    IO::CAN::CANStatus requestVnVp(uint16_t *voltageN, uint16_t *voltageP);
+
+    IO::CAN::CANStatus requestBatteryVoltage(uint16_t *batteryVoltage);
+
+    IO::CAN::CANStatus restartGFDB();
+
+    IO::CAN::CANStatus setMaxDesignVoltage(uint16_t *maxVoltage);
+
+
+
+private:
+    IO::CAN& can;
+
+    IO::CAN::CANStatus transmitMessage(uint8_t *payload, size_t len);
+
+    IO::CAN::CANStatus receiveMessage(uint8_t *payload, size_t len);
+};
+
+}
+
+#endif//PVC_GFDB_H
---
+++ src/PreCharge/GFDB.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aaron on 1/30/23.
+//
+
+#include "GFDB.h"
