46f87f02d2d08eaf1dcf13ba86ab4216bca49035
GitHub Build <N/A>
2023-01-31 00:43:30 +0000

    Applied Formatting Changes During GitHub Build

--- targets/MAX22530/main.cpp
+++ targets/MAX22530/main.cpp
@@ -19,19 +19,19 @@ IO::GPIO* devices[deviceCount];
 
 int main() {
     // Setup IO
-   IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
-   // Setup SPI
-   devices[0] = &IO::getGPIO<IO::Pin::SPI_CS>(EVT::core::IO::GPIO::Direction::OUTPUT);
-   devices[0]->writePin(IO::GPIO::State::HIGH);
-   IO::SPI& spi = IO::getSPI<IO::Pin::SPI_SCK, IO::Pin::SPI_MOSI, IO::Pin::SPI_MISO>(devices, deviceCount);
-   spi.configureSPI(SPI_SPEED, SPI_MODE0, SPI_MSB_FIRST);
+    // Setup SPI
+    devices[0] = &IO::getGPIO<IO::Pin::SPI_CS>(EVT::core::IO::GPIO::Direction::OUTPUT);
+    devices[0]->writePin(IO::GPIO::State::HIGH);
+    IO::SPI& spi = IO::getSPI<IO::Pin::SPI_SCK, IO::Pin::SPI_MOSI, IO::Pin::SPI_MISO>(devices, deviceCount);
+    spi.configureSPI(SPI_SPEED, SPI_MODE0, SPI_MSB_FIRST);
 
     PreCharge::MAX22530 MAX(spi);
 
-   while (1) {
-       for (int reg = 0x01; reg <= 0x04; reg++) {
-           uart.printf("Register 0x%x: %d\r\n", reg, MAX.readVoltage(reg));
-       }
-   }
+    while (1) {
+        for (int reg = 0x01; reg <= 0x04; reg++) {
+            uart.printf("Register 0x%x: %d\r\n", reg, MAX.readVoltage(reg));
+        }
+    }
 }
