fe924bdb310c12f33610f2f57180373e1e0ed04c
mjmagee991 <mjmagee991@gmail.com>
2023-03-30 17:06:10 -0400

    Added Contactor Driver

---
+++ include/PreCharge/dev/Contactor.hpp
@@ -0,0 +1,23 @@
+#ifndef PRE_CHARGE_INCLUDE_PRECHARGE_DEV_CONTACTOR_HPP
+#define PRE_CHARGE_INCLUDE_PRECHARGE_DEV_CONTACTOR_HPP
+
+#include <EVT/io/GPIO.hpp>
+
+namespace IO = EVT::core::IO;
+
+namespace PreCharge {
+
+class Contactor {
+public:
+    Contactor(IO::GPIO& cont1, IO::GPIO& cont2);
+
+    void setOpen(bool isOpen);
+
+private:
+    IO::GPIO& cont1;
+    IO::GPIO& cont2;
+};
+
+}
+
+#endif//PRE_CHARGE_INCLUDE_PRECHARGE_DEV_CONTACTOR_HPP
---
+++ src/PreCharge/dev/Contactor.cpp
@@ -0,0 +1,21 @@
+#include <PreCharge/dev/Contactor.hpp>
+#include <EVT/utils/time.hpp>
+
+namespace time = EVT::core::time;
+
+PreCharge::Contactor::Contactor(IO::GPIO& cont1, IO::GPIO& cont2) : cont1(cont1), cont2(cont2) {
+    cont1.writePin(IO::GPIO::State::LOW);
+    cont2.writePin(IO::GPIO::State::LOW);
+}
+
+void PreCharge::Contactor::setOpen(bool isOpen) {
+    if (isOpen) {
+        cont1.writePin(IO::GPIO::State::HIGH);
+        time::wait(20);
+        cont1.writePin(IO::GPIO::State::LOW);
+    } else {
+        cont2.writePin(IO::GPIO::State::HIGH);
+        time::wait(20);
+        cont2.writePin(IO::GPIO::State::LOW);
+    }
+}
